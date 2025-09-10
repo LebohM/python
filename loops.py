6293927a9418702664ecc0268cf018c29d1bc138
LebohM <46931086+LebohM@users.noreply.github.com>
2025-09-10 16:25:15 +0200

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,1 +1,23 @@
-#Loops
+# FOR Loop
+
+fruits = ["Banana","Apple", "Orange"]
+print(fruits)
+
+numbers = [2,4,6,9]
+print(numbers)
+
+
+for f in fruits:
+    print(f)
+    
+for n in numbers:
+    print(n)
+    
+
+#While Loop
+
+c = 1
+
+while c <= 5:
+    print(c)
+    c += 1 #increment
