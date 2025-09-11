ded16f1c9911afcd820bb027956a8d34d1400a41
LebohM <46931086+LebohM@users.noreply.github.com>
2025-09-11 21:01:45 +0200

    Update lists.py

--- lists.py
+++ lists.py
@@ -2,7 +2,14 @@ fruits = ["Banana", "Orange", "Cherry", "Apple", "Berry"]
 
 print(fruits[2])
 
-fruits[4] = "blueberry"
+fruits[4] = "Blueberry"
 print()
 
 print(fruits)
+
+#Edit lists
+print()
+fruits.append("Kiwi")
+print(fruits)
+
+fruits.insert(2)
