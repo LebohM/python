92425c272bee3a0c2eab74cd42b3a086a1537676
LebohM <46931086+LebohM@users.noreply.github.com>
2025-09-12 09:52:29 +0200

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -2,3 +2,28 @@
 # but they are immutable/they cannot be changed, added or removed.
 
 myTupples = (1,2,3,4,5)
+print(myTupples)
+
+print()
+print(myTupples[0])
+
+print()
+print(myTupples[3])
+
+print()
+print(myTupples[-1]) #Get the last item on the list or tupple
+
+#concatenating and repeating/duplicating tupples
+
+tupple1 = (1,2,3)
+tupple2 = (4,5,6)
+
+conc_tupple = tupple1 + tupple2
+
+rep_tupple = tupple2 * 3
+
+print()
+print(conc_tupple)
+
+print()
+print(rep_tupple)
