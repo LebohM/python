1a2d8fbd86ddf34e456f642e045775410e15e76d
LebohM <46931086+LebohM@users.noreply.github.com>
2025-09-12 20:04:32 +0200

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,9 @@
+#sets are unodered items that cannot be changed but can be removed or added.
+#they do not allow duplicates
+
+my_set = {3,4,2,1}
+
+print(my_set, "\n")
+
+my_set.add(0)
+print(my_set, "\n")
