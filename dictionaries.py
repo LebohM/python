5adb1643efceaccf1901bb1fd1611279e157c578
LebohM <46931086+LebohM@users.noreply.github.com>
2025-09-13 17:05:28 +0200

    Update dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,11 @@
+#dictionaries are a collection of key value pairs,  the key value pare are separated by a colon: and can be of any data types
+
+myDictionary = {"Apples":3, "Mango":2, "Banana":6}
+
+print(myDictionary["Mango"], "\n")
+
+myDictionary["grapes"] = 5
+print(myDictionary, '\n')
+
+myDictionary["Apples"] = 7
+print(myDictionary, "\n")
