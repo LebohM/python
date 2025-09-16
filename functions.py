034368d10dbb99d93c924a0bf8cf1b800fe1bb1c
LebohM <46931086+LebohM@users.noreply.github.com>
2025-09-16 09:50:35 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-
+'''
 def greet(name):
     print(f"hello, {name}", '\n')
     
@@ -8,5 +8,16 @@ def addNums(a, b):
     return a + b
 
 results = addNums(2, 7)
+
+print(results)
+'''
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+results = factorial(4)
 
 print(results)
